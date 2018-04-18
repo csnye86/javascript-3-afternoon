2a06769ce5592327b2b892437adcf9d5f34cb4d4
Cameron Nye <home@Camerons-MacBook-Pro.local>
2018-04-18 14:47:57 -0600

    1-2 done

--- classes.js
+++ classes.js
@@ -29,7 +29,25 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor (first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age
+  }
+  
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`;
+
+  }
+}
+
+
+let cameron = new Employee('Cameron','Nye','csnye86@yahoo.com',31);
+console.log(cameron.makeWidget());
+console.log(cameron)
+
 
 
 
@@ -49,7 +67,26 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = []
+  }
+
+  hire(employee){
+    this.reports.push(employee);
+  }
+
+  fire(index){
+    this.reports.splice([index], 1);
+  }
+}
+let athena = new Manager('Athena','Nye','athena.alvarez@gmail.com',27)
+console.log(Manager)
+
 
 
 
